5281594f86d030410af0a608b60129acecb1c730
GeorgeSemenov <zhora.sem@yandex.ru>
2024-02-13 06:02:42 +0300

    almost done

--- source/blocks/request-form/request-form.js
+++ source/blocks/request-form/request-form.js
@@ -1,2 +1,57 @@
 $(document).ready(function(){
+	const RequestFromSubmitBtn = document.getElementById("request-form-submit-button");
+	RequestFromSubmitBtn.addEventListener('click',(e)=>{
+		e.preventDefault();
+		
+		const notaficationBG = document.createElement("div");
+		notaficationBG.style.position = "abolute";
+		notaficationBG.style.top = "0px";
+		notaficationBG.style.right = "0px";
+		notaficationBG.style.bottom = "0px";
+		notaficationBG.style.left = "0px";
+		notaficationBG.style.backgroundColor = "grey";
+
+		document.body.appendChild(notaficationBG)
+	})
+
 })
+
+
+// Создаем новый элемент div
+const newDiv = document.createElement("div");
+
+// Применяем CSS свойства к новому div
+newDiv.style.width = "200px"; // Ширина 200 пикселей
+newDiv.style.height = "100px"; // Высота 100 пикселей
+newDiv.style.backgroundColor = "lightblue"; // Цвет фона светло-голубой
+newDiv.style.border = "1px solid black"; // Граница 1 пиксель черной цветом
+
+// Добавляем новый div на страницу
+document.body.appendChild(newDiv);
+
+//удаляем элемент
+// Находим ранее созданный div
+const divToRemove = document.querySelector("div");
+
+// Проверяем, что div существует
+if (divToRemove) {
+    // Удаляем div из его родительского элемента
+    divToRemove.parentNode.removeChild(divToRemove);
+} else {
+    console.log("Элемент не найден");
+}
+
+//Не забудь удалить и слушателей   removeListener
+// Создаем функцию-обработчик для события click
+function handleClick() {
+    console.log("Клик по элементу");
+}
+
+// Находим элемент, к которому добавлен слушатель
+const element = document.querySelector("#myElement");
+
+// Добавляем слушатель события click
+element.addEventListener("click", handleClick);
+
+// Удаляем слушатель события click
+element.removeEventListener("click", handleClick);
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -2,3 +2,4 @@ import './index.scss';
 import '../../JS-components/libs.js';
 import '../../blocks/header/header.js';
 import '../../blocks/footer/footer.js';
+import '../../blocks/request-form/request-form.js';
